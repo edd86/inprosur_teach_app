4048cdac484aa0e10b583cc3c55ef4a33abc5847
edd86 <edward.cristob@gmail.com>
2025-08-27 18:38:51 -0400

    feat(routing): add config and profile page routes
    Add new route constants and mappings for config and profile pages to enable navigation to these features.

--- lib/core/constants/app_routes.dart
+++ lib/core/constants/app_routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:inprosur_teach_app/presentation/pages/buy/buy_page.dart';
+import 'package:inprosur_teach_app/presentation/pages/config/config_page.dart';
 import 'package:inprosur_teach_app/presentation/pages/courses/course_page.dart';
 import 'package:inprosur_teach_app/presentation/pages/home/home_page.dart';
 import 'package:inprosur_teach_app/presentation/pages/login/login_page.dart';
+import 'package:inprosur_teach_app/presentation/pages/profile/profile_page.dart';
 import 'package:inprosur_teach_app/presentation/pages/search/search_page.dart';
 import 'package:inprosur_teach_app/presentation/pages/search/search_page_by_category.dart';
 import 'package:inprosur_teach_app/presentation/pages/student/student_data_page.dart';
@@ -15,11 +17,15 @@ class AppRoutes {
   static String searchPageByCategory = '/searchByCategory';
   static String coursePage = '/coursePage';
   static String buyPage = '/buyPage';
+  static String configPage = '/configPage';
+  static String profilePage = '/profilePage';
 
   static final Map<String, WidgetBuilder> routes = {
     homePage: (context) => HomePage(),
     login: (context) => LoginPage(),
     searchPage: (context) => SearchPage(),
+    configPage: (context) => ConfigPage(),
+    profilePage: (context) => ProfilePage(),
   };
 
   static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
